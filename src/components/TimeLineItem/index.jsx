5549586a6def53321eee82e3dba3f7c5b6c58084
cuonglm <cuonglm@fpt.com>
2024-04-25 09:28:35 +0700

    fix paddding

--- src/components/TimeLineItem/index.jsx
+++ src/components/TimeLineItem/index.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 export default function TimeLineItem(props) {
 	const { label, content, img } = props;
 	return (
-		<div className="d-flex align-items-center justify-content-center mt-5 timeline-item_container">
+		<div className="d-flex align-items-center justify-content-center mt-5 timeline-item_container px-2">
 			<div className="timeline-item_text_container">
 				<label>{label}</label>
 				<p className="mt-2">{content}</p>
